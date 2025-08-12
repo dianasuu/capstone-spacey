695ced1ab2638e0540b7663681294c7ab609945c
dianasuu <dianesuu@gmail.com>
2025-08-12 17:24:14 +0500

    Update capstoney.py

--- capstoney.py
+++ capstoney.py
@@ -1,1 +1,10 @@
-f
+!pip3 install beautifulsoup4
+!pip3 install requests
+!pip install pandas
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import unicodedata
+import pandas as pd
